d25263738d10444ad570474433b3b6ab6349e112
Yasin Kaya <yasinkaya.121@gmail.com>
2020-02-18 16:47:08 +0300

    crc fix

--- crc16.py
+++ crc16.py
@@ -1,39 +1,39 @@
-    
-    class CRC16:
-    
-        CRC16_CCITT = 0x1021    #X.25, V.41, HDLC FCS, Bluetooth, ...
+   
+class CRC16:
 
-        def CRC16(self):
-            #default constructer
+    CRC16_CCITT = 0x1021    #X.25, V.41, HDLC FCS, Bluetooth, ...    
 
-        # crc16 base function 
-        def crc16(self, crcValue, newByte):
+    def CRC16(self):
+        #default constructer
 
-            for i in range(8):
+    # crc16 base function 
+    def crc16(self, crcValue, newByte):
 
-                if ((((crcValue & 0x8000) >> 8) ^ (newByte & 0x80)) > 0)
-                {
-                    crcValue = (crcValue << 1) ^ CRC16_CCITT
-                }
-                else
-                {
-                    crcValue = crcValue << 1
-                }
+        for i in range(8):
 
-                newByte <<= 1
+            if ((((crcValue & 0x8000) >> 8) ^ (newByte & 0x80)) > 0)
+            {
+                crcValue = (crcValue << 1) ^ self.CRC16_CCITT
+            }
+            else
+            {
+                crcValue = crcValue << 1
+            }
+
+            newByte <<= 1
+        
+        return crcValue
+        
+
+    # calculate crc function.
+    def exampleOfUseCRC16(self, Data, len):
+        
+        aux = 0
+        crc = 0xFFFF  # Initialization of crc to 0xFFFF for CCITT
+
+        while (aux < len):
             
-            return crcValue
-            
+            crc = crc16(crc, Data[aux])
+            aux += 1
 
-        # calculate crc function.
-        def exampleOfUseCRC16(self, Data, len):
-            
-            aux = 0
-            crc = 0xFFFF  # Initialization of crc to 0xFFFF for CCITT
-
-            while (aux < len):
-                
-                crc = crc16(crc, Data[aux])
-                aux += 1
-
-            return crc
+        return crc
