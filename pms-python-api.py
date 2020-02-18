7219c0b695d193b05adf76cbe36dac4d417957de
Yasin Kaya <yasinkaya.121@gmail.com>
2020-02-18 14:32:51 +0300

    created class

--- pms-python-api.py
+++ pms-python-api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import smbus2
+import RPi.GPIO as GPIO
 import time
 
 bus = smbus2.SMBus(1)
@@ -23,59 +24,99 @@ COMMAND_TYPE_RESPONSE =				0x03
 
 DEVICE_ADDRESS = 0x41      #7 bit address (will be left shifted to add the read write bit)
 
+###########################################
+### Private Methods #######################
+###########################################
+
+# Function for printing debug message 
+def debug_print(message):
+	print(message)
+
+# Function for getting time as miliseconds
+def millis():
+	return int(time.time())
+
+# Function for delay as miliseconds
+def delay(ms):
+	time.sleep(float(ms/1000.0))
+
 #############################################################
-### Public Functions ########################################
+### PMS CLASS ###############################################
 #############################################################
-def sendCommand():
-	global bufferSend
-	print(bufferSend)
-	bus.write_i2c_block_data(DEVICE_ADDRESS, 0x01, bufferSend)
+class SixfabPMS:
+	board = "Sixfab Raspberry Pi UPS HAT"
+	
+	# Special Characters
+	CTRL_Z = '\x1A'
+	
+	# Initializer function
+	def __init__(self):
+		debug_print(self.board + " Class initialized!")
+ 		
+	def __del__(self): 
+		self.clearGPIOs()
+			
+	# Function for clearing GPIO's setup
+	def clearGPIOs(self):
+		GPIO.cleanup()
 
-def checkCommand(recievedByte):
-	global bufferRecieve
-	global bufferRecieveIndex
-	datalen = 0
 
-	if(bufferRecieveIndex == 0 and recievedByte != START_BYTE_RECIEVED):
-		return
+	#############################################################
+	### I2C Protocol Functions ##################################
+	#############################################################
+	def sendCommand(self):
+		global bufferSend
+		print(bufferSend)
+		bus.write_i2c_block_data(DEVICE_ADDRESS, 0x01, bufferSend)
+
+	def checkCommand(self, recievedByte):
+		global bufferRecieve
+		global bufferRecieveIndex
+		datalen = 0
+
+		if(bufferRecieveIndex == 0 and recievedByte != START_BYTE_RECIEVED):
+			return
+			
+		bufferRecieve.append(recievedByte)
+		bufferRecieveIndex += 1
 		
-	bufferRecieve.append(recievedByte)
-	bufferRecieveIndex += 1
-	
-	if(bufferRecieveIndex < PROTOCOL_HEADER_SIZE):
-		return
-	
-	datalen = (bufferRecieve[3] << 8) | bufferRecieve[4]
-	datalen = datalen / 2
-
-	if(bufferRecieveIndex == (PROTOCOL_FRAME_SIZE + datalen)):
-		print('[{}]'.format(', '.join(hex(x) for x in bufferRecieve)))
-		bufferRecieveIndex = 0
-	
-	
-def recieveCommand(lenOfResponse):
-	global bufferRecieve
+		if(bufferRecieveIndex < PROTOCOL_HEADER_SIZE):
+			return
 		
-	for i in range(lenOfResponse):
-		c = bus.read_byte(DEVICE_ADDRESS)
-		print("Recieved byte: " + str(hex(c)))
-		checkCommand(c)
+		datalen = (bufferRecieve[3] << 8) | bufferRecieve[4]
+		datalen = datalen / 2
 
-def createDummyData(command):
-	global bufferSend
-	bufferSend.append(START_BYTE_SENT)
-	bufferSend.append(command)
-	bufferSend.append(COMMAND_TYPE_REQUEST)
-	bufferSend.append(0x00)
-	bufferSend.append(0x00)
-	bufferSend.append(0x05)
-	bufferSend.append(0x04)
+		if(bufferRecieveIndex == (PROTOCOL_FRAME_SIZE + datalen)):
+			print('[{}]'.format(', '.join(hex(x) for x in bufferRecieve)))
+			bufferRecieveIndex = 0
+		
+		
+	def recieveCommand(self, lenOfResponse):
+		global bufferRecieve
+			
+		for i in range(lenOfResponse):
+			c = bus.read_byte(DEVICE_ADDRESS)
+			print("Recieved byte: " + str(hex(c)))
+			checkCommand(c)
+
+	def createDummyData(self, command):
+		global bufferSend
+		bufferSend.append(START_BYTE_SENT)
+		bufferSend.append(command)
+		bufferSend.append(COMMAND_TYPE_REQUEST)
+		bufferSend.append(0x00)
+		bufferSend.append(0x00)
+		bufferSend.append(0x05)
+		bufferSend.append(0x04)
 
 # Example Code Area
-createDummyData(1)
-sendCommand()
+
+pms = SixfabPMS()
+
+pms.createDummyData(1)
+pms.sendCommand()
 time.sleep(0.1)
-recieveCommand(16)
+pms.recieveCommand(16)
 # End of Example Code Area	
 
 	
