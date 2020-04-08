1a27ca7b4d4d3fbdb0920aea8dd128ec19de2032
Yasin Kaya <yasinkaya.121@gmail.com>
2020-04-08 18:38:52 +0300

    added example for understanding epoch and local epoch

--- example5.py
+++ example5.py
@@ -3,6 +3,18 @@ import time
 
 pms = SixfabPMS()
 
-print(pms.getButton1Status())
-#print(pms.anySoftActionIsExist())
-print(pms.getBatteryDesignCapacity())
+epoch = time.time() # to get timestamp in seconds in GMT0
+
+# to get local time on raspberry pi 
+localtime = time.asctime( time.localtime(time.time()) )
+print("Local current time :", localtime)
+
+# to get timezone difference as minus 
+print(time.timezone)
+
+# standard epoch time with no location specific
+print("Epoch Time: " + str(int(epoch)))
+
+# to calculate epoch with respect to local time
+# use it for setRTCTime() function as timestamp (epoch - time.timezone)
+print("Epoch Local: " + str(int(epoch) - time.timezone))
